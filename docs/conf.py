273dc5adf9d64d7ce5f52c9e13ad191bbb07ca59
e-cohen <e.cohen@salesforce.com>
2020-10-08 00:19:03 +0300

    project name

--- docs/conf.py
+++ docs/conf.py
@@ -43,8 +43,8 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'Read the Docs Template'
-copyright = u'2014, Read the Docs'
+project = u'Molecular Conceptor'
+copyright = u'Synergix'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
